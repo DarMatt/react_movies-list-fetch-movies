68b46d89f7a8e9d9a6819a4251014e92dfc9aec9
DarMatt <daryamatviienko@gmail.com>
2021-01-30 16:06:50 +0200

    Updates

---
+++ precache-manifest.0b5dc2fb2b8a11888afc5ac5d641b222.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c19000b3e116153c64e643c24fadcc28",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3854b9d14c856b4c11fe",
+    "url": "./static/css/2.9634ee28.chunk.css"
+  },
+  {
+    "revision": "7b6c5b4350fd28883592",
+    "url": "./static/css/main.6ce43aae.chunk.css"
+  },
+  {
+    "revision": "3854b9d14c856b4c11fe",
+    "url": "./static/js/2.bb4f46d8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.bb4f46d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b6c5b4350fd28883592",
+    "url": "./static/js/main.d40dddca.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
